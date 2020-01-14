6bd0cec2f0c36162e98dd50141d4cc7af995f301
Giorgio <giorgiocolomb@gmail.com>
2020-01-14 11:40:32 +0100

    first html

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Spazi Sostenibili | Blog Architettura Milanese</title>
+    <link rel="stylesheet" href="header.css">
+    <link href="https://fonts.googleapis.com/css?family=Prata&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="container_text_header">
+            <div class="spazi"><p>SPAZI</p>
+                <div class="box_text_header"></div>
+            </div>
+            <p class="sostenibili">SOSTENIBILI</p>
+        </div>
+    </header>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+<body>
+    <?php include "header.php"?>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique corporis deserunt possimus, itaque vitae fugiat vel optio illum hic, quisquam sit explicabo neque delectus excepturi! Cupiditate harum nulla deleniti nemo!
+    Sed pariatur aspernatur praesentium tempore ipsum dolor distinctio, est molestias sint aliquam et totam. Iste neque doloribus dolores unde fuga aliquam at, quae eos tenetur, iure esse dolor molestiae reiciendis.
+    Molestiae, atque numquam harum sequi quo fugit molestias provident quod dolorem, magni neque officiis aliquam vero ipsum voluptatem! Harum culpa vel voluptatibus iusto ipsa. Enim voluptate architecto ut maiores quis.</p>
+</body>
+</html>
