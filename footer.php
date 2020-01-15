4e48fd0dfbe632d14872e74049c2f03e63b031e8
giuseppefigliuolo <57524880+giuseppefigliuolo@users.noreply.github.com>
2020-01-15 20:25:05 +0100

    Caricamento footer.php e suo css
    Il footer è completo e responsive, va solo abbellito un po' in formato desktop (vanno aggiustate le proporzioni). In più carico 3 svg dei social presenti nel footer.

--- footer.php
+++ footer.php
@@ -2,8 +2,23 @@
 <html lang="it">
 <head>
     <link rel="stylesheet" href="footer.css">
+    <link href="https://fonts.googleapis.com/css?family=Prata&display=swap" rel="stylesheet">
 </head>
 <body>
-
+    <footer>
+    <img src="img/full_logo.svg" alt="logo Spazi Sostenibili">
+        <div class="nav-menu">
+            <p><a href="#">Home</a></p>
+            <p><a href="#">Team</a></p>
+            <p><a href="#">Archivio</a></p>
+        </div>
+        <ul>
+            <li><a href="#"><img src="img/instagram-logo.svg" alt="Spazi Sostenibili Instagram"></a></li>
+            <li><a href="#"><img src="img/facebook-logo.svg" alt="Spazi Sostenibili Facebook"></a></li>
+            <li><a href="#"><img src="img/linkedin-logo.svg" alt="Spazi Sostenibili Linked In"></a></li>
+        </ul>
+        <p class="footer-diritti">Il sito è stato creato a scopo didattico dagli allievi del corso accademico primo anno della <span>Scuola Mohole di Web e Digital Media</span>. I diritti dei contenuti e dei marchi citati appartengono ai loro legittimi proprietari.</p>
+        
+    </footer >
 </body>
 </html>
