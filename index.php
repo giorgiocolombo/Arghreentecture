6ffd5a1122739aa11a783ce47bf7f67751410ff5
Giorgio <giorgiocolomb@gmail.com>
2020-01-19 20:30:31 +0100

    figcaption

--- index.php
+++ index.php
@@ -21,23 +21,28 @@
     <section class="gallery_posts">
         <figure class="gallery_item gallery_item_1">
             <div class="gallery_cover"></div>
-                <img src="img-samples/photo-1579126999978-eac334843123.jpg" alt="Gallery 1" class="gallery__img">
+            <p class="figcaption">Nome Gallery</p>
+            <img src="img-samples/photo-1579126999978-eac334843123.jpg" alt="Gallery 1" class="gallery_img">
         </figure>
         <figure class="gallery_item gallery_item_2">
             <div class="gallery_cover"></div>
-                <img src="img-samples/photo-1579160855213-a6e60979eecf.jpg" alt="Gallery 2" class="gallery__img">
+            <p class="figcaption">Nome Gallery</p>
+            <img src="img-samples/photo-1579160855213-a6e60979eecf.jpg" alt="Gallery 2" class="gallery_img">
         </figure>
         <figure class="gallery_item gallery_item_3">
             <div class="gallery_cover"></div>
-                <img src="img-samples/photo-1579189880841-43ddba8d8efd.jpg" alt="Gallery 3" class="gallery__img">
+            <p class="figcaption">Nome Gallery</p>
+            <img src="img-samples/photo-1579189880841-43ddba8d8efd.jpg" alt="Gallery 3" class="gallery_img">
         </figure>
         <figure class="gallery_item gallery_item_4">
             <div class="gallery_cover"></div>
-                <img src="img-samples/photo-1579191748111-90d7f89fe214 (1).jpg" alt="Gallery 4" class="gallery__img">
+            <p class="figcaption">Nome Gallery</p>
+            <img src="img-samples/photo-1579191748111-90d7f89fe214 (1).jpg" alt="Gallery 4" class="gallery_img">
         </figure>
         <figure class="gallery_item gallery_item_5">
             <div class="gallery_cover"></div>
-                <img src="img-samples/photo-1579194044198-cdd94102ef46.jpg" alt="Gallery 5" class="gallery__img">
+            <p class="figcaption">Nome Gallery</p>
+            <img src="img-samples/photo-1579194044198-cdd94102ef46.jpg" alt="Gallery 5" class="gallery_img">
         </figure>
     </section>
     
