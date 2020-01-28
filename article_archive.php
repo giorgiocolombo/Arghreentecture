cdd6619e3d0923ae9b185f6394d7a63013720b94
Giorgio <giorgiocolomb@gmail.com>
2020-01-28 13:00:11 +0100

    a

--- article_archive.php
+++ article_archive.php
@@ -9,84 +9,19 @@
                 <p>#bubu</p>
             </div>
             <h2>Titolo articolo di Spazi Sostenibili</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit modi expedita quod aut, nihil iste, hic quidem laborum nesciunt nemo nisi totam omnis, animi aspernatur dolorem ex. Aperiam, accusamus aliquid. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil adipisci quam fuga architecto soluta, velit quaerat libero nostrum hic vero perspiciatis labore doloribus aut doloremque quod, vitae dolore saepe aliquid.</p>
+            <p class="text_card_p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit modi expedita quod aut, nihil iste, hic quidem laborum nesciunt nemo nisi totam omnis, animi aspernatur dolorem ex. Aperiam, accusamus aliquid. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil adipisci quam fuga architecto soluta, velit quaerat libero nostrum hic vero perspiciatis labore doloribus aut doloremque quod, vitae dolore saepe aliquid.</p>
             <div class="interaction">
                 <div class="button_article">Continua a leggere</div>
-                <img src="" alt="">
-                <img src="" alt="">
+                <img src="img/heart.svg" alt="">
+                <img src="img/share.svg" alt="">
             </div>
         </div>
         <div class="pic_card_container">
-            <!-- <img class="pic_card" src="img-samples/photo-1579126999978-eac334843123.jpg"> -->
+            <img class="pic_card" src="img-samples/photo-1579126999978-eac334843123.jpg">
         </div>
-        <div class="green_card">a</div>
-        <div class="pink_card">a</div>
+        <div class="green_card"></div>
+        <div class="pink_card"></div>
     </div>
-    <div class="container_article">
-        <div class="text_card">
-            <div class="tags">
-                <p>#blabla</p>
-                <p>#bubu</p>
-                <p>#blabla</p>
-                <p>#bubu</p>
-            </div>
-            <h2>Titolo articolo di Spazi Sostenibili</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit modi expedita quod aut, nihil iste, hic quidem laborum nesciunt nemo nisi totam omnis, animi aspernatur dolorem ex. Aperiam, accusamus aliquid. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil adipisci quam fuga architecto soluta, velit quaerat libero nostrum hic vero perspiciatis labore doloribus aut doloremque quod, vitae dolore saepe aliquid.</p>
-            <div class="interaction">
-                <div class="button_article">Continua a leggere</div>
-                <img src="" alt="">
-                <img src="" alt="">
-            </div>
-        </div>
-        <div class="pic_card_container">
-            <!-- <img class="pic_card" src="img-samples/photo-1579126999978-eac334843123.jpg"> -->
-        </div>
-        <div class="green_card">a</div>
-        <div class="pink_card">a</div>
-    </div>
-    <div class="container_article">
-        <div class="text_card">
-            <div class="tags">
-                <p>#blabla</p>
-                <p>#bubu</p>
-                <p>#blabla</p>
-                <p>#bubu</p>
-            </div>
-            <h2>Titolo articolo di Spazi Sostenibili</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit modi expedita quod aut, nihil iste, hic quidem laborum nesciunt nemo nisi totam omnis, animi aspernatur dolorem ex. Aperiam, accusamus aliquid. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil adipisci quam fuga architecto soluta, velit quaerat libero nostrum hic vero perspiciatis labore doloribus aut doloremque quod, vitae dolore saepe aliquid.</p>
-            <div class="interaction">
-                <div class="button_article">Continua a leggere</div>
-                <img src="" alt="">
-                <img src="" alt="">
-            </div>
-        </div>
-        <div class="pic_card_container">
-            <!-- <img class="pic_card" src="img-samples/photo-1579126999978-eac334843123.jpg"> -->
-        </div>
-        <div class="green_card">a</div>
-        <div class="pink_card">a</div>
-    </div>
-    <div class="container_article">
-        <div class="text_card">
-            <div class="tags">
-                <p>#blabla</p>
-                <p>#bubu</p>
-                <p>#blabla</p>
-                <p>#bubu</p>
-            </div>
-            <h2>Titolo articolo di Spazi Sostenibili</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit modi expedita quod aut, nihil iste, hic quidem laborum nesciunt nemo nisi totam omnis, animi aspernatur dolorem ex. Aperiam, accusamus aliquid. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil adipisci quam fuga architecto soluta, velit quaerat libero nostrum hic vero perspiciatis labore doloribus aut doloremque quod, vitae dolore saepe aliquid.</p>
-            <div class="interaction">
-                <div class="button_article">Continua a leggere</div>
-                <img src="" alt="">
-                <img src="" alt="">
-            </div>
-        </div>
-        <div class="pic_card_container">
-            <!-- <img class="pic_card" src="img-samples/photo-1579126999978-eac334843123.jpg"> -->
-        </div>
-        <div class="green_card">a</div>
-        <div class="pink_card">a</div>
-    </div>
+    
 </section>
 <?php include "footer.php"?>
