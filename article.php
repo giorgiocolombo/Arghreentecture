13bfb93aa07a2c8b1c0a5cca95774b0d6336d776
Giorgio <giorgiocolomb@gmail.com>
2020-02-02 17:26:49 +0100

    some fixes and article animations

--- article.php
+++ article.php
@@ -1,23 +1,25 @@
 <?php include "header.php"?>
 
 <div id="single">
-    <div class="banner"></div>
-    <div class="date-n-author">
-        <p>31 Gennaio 2020</p>
-        <p>di <span>Giuseppe Figliuolo</span></p>
+    <div class="banner" data-aos="fade" data-aos-duration="900"></div>
+    <div class="single_container" data-aos="fade-up" data-aos-duration="600">
+        <div class="date-n-author">
+            <p>31 Gennaio 2020</p>
+            <p>di <span>Giuseppe Figliuolo</span></p>
+        </div>
+
+        <h1>Il nuovo <br>
+            Parco Monsanto: <br>
+            120 ettari di biodiversità</h1>
+
+        <div class="line-divider"></div>
+        
+        <div class="article-text">
+            <p>Il sindaco di Milano Giuseppe Sala dichiara guerra allo smog. Con il blocco totale del traffico previsto per domenica 2 febbraio, ma anche con una crociata al riscaldamento che inquina e, in particolare, contro le caldaie a gasolio. E se per l’assessore lombardo all’Ambiente <span>Raffaele Cattaneo</span>, lo stop alle auto è “un esempio di demagogia in salsa green”, mentre più utile sarebbe proprio “sostituire le caldaie che hanno più di quindici anni”, per la onlus <span>Cittadini per l’Aria</span> quello dell’assessore regionale è un tentativo di “spostare l’attenzione da un tema centrale, come quello del traffico, che però allontana gli elettori.</p>
+            <p>Il blocco di domenica è un <span>provvedimento certamente insufficiente</span> – spiega la onlus – ma necessario”. D’altro canto, un recente progetto del costruttore di caldaie Vaillant e dell’Università di Milano ha rivelando che ogni italiano produce in media ogni anno <span>5 tonnellate e mezza di CO2:</span> oltre un terzo dai trasporti, un altro terzo dall’alimentazione e dai rifiuti, il resto dal riscaldamento (25%) e illuminazione ed elettrodomestici (5%).</p>    
+
+        </div>
     </div>
-
-    <h1>Il nuovo <br>
-        Parco Monsanto: <br>
-        120 ettari di biodiversità</h1>
-
-    <div class="line-divider"></div>
-    
-    <div class="article-text">
-        <p>Il sindaco di Milano Giuseppe Sala dichiara guerra allo smog. Con il blocco totale del traffico previsto per domenica 2 febbraio, ma anche con una crociata al riscaldamento che inquina e, in particolare, contro le caldaie a gasolio. E se per l’assessore lombardo all’Ambiente <span>Raffaele Cattaneo</span>, lo stop alle auto è “un esempio di demagogia in salsa green”, mentre più utile sarebbe proprio “sostituire le caldaie che hanno più di quindici anni”, per la onlus <span>Cittadini per l’Aria</span> quello dell’assessore regionale è un tentativo di “spostare l’attenzione da un tema centrale, come quello del traffico, che però allontana gli elettori.</p>
-        <p>Il blocco di domenica è un <span>provvedimento certamente insufficiente</span> – spiega la onlus – ma necessario”. D’altro canto, un recente progetto del costruttore di caldaie Vaillant e dell’Università di Milano ha rivelando che ogni italiano produce in media ogni anno <span>5 tonnellate e mezza di CO2:</span> oltre un terzo dai trasporti, un altro terzo dall’alimentazione e dai rifiuti, il resto dal riscaldamento (25%) e illuminazione ed elettrodomestici (5%).</p>    
-
-</div>
 
 
 <?php include "footer.php"?>
--- gallery_archive.php
+++
@@ -1,7 +0,0 @@
-<?php include "header.php"?>
-<h1> <br>
-<br>
-<br>
-<br>
-<br> Gallery</h1>
-<?php include "footer.php"?>
