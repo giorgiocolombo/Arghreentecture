3563de882a6bbf2feb473fab5010cf745b970b9b
giuseppefigliuolo <figliuologiuseppe@gmail.com>
2020-02-28 18:39:03 +0100

    Aggiornamento al sito statico
    durante la quarantena da covid19

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
                     <div class="box_text_header"></div>
                 </div>
                 <p class="sostenibili">SOSTENIBILI</p>
+                <p class="payoffDesktop">Prospettive green a Milano</p>
             </div>
             <img data-aos="fade-left" data-aos-duration="300" data-aos-anchor-placement="top-center" data-aos-offset="-500" src="img/bosco.jpg" alt="Bosco verticale" id="bosco2">
             <img data-aos="fade-left" data-aos-duration="300" data-aos-anchor-placement="top-center" data-aos-offset="-500" src="img/bosco.jpg" alt="Bosco verticale" id="bosco1">
@@ -22,35 +23,40 @@
             <div class="container_article ca_1" data-aos="fade-up" data-aos-duration="600">
                 <div class="text_card">
                     <div class="tags">
-                        <p>Autore</p>
-
+                        <p>di <a href="about.php">Giorgio Colombo</a></p>
                     </div>
-                    <h2>Titolo articolo di Spazi Sostenibili</h2>
-                    <p class="text_card_p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit modi expedita quod aut, nihil iste, hic quidem laborum nesciunt nemo nisi totam omnis, animi aspernatur dolorem ex. Aperiam, accusamus aliquid. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil adipisci quam fuga architecto soluta, velit quaerat libero nostrum hic vero perspiciatis labore doloribus aut doloremque quod, vitae dolore saepe aliquid.</p>
+                    <h2>Gioia22 stravolge l'impatto ambientale</h2>
+                    <p class="text_card_p">Si tratta dell’innovativa torre in costruzione che nasce all’interno del progetto
+                        di riqualificazione di Porta Nuova e si impone come nuovo standard di
+                        ecosostenibilità a Milano. Il nuovo grattacielo sostituisce il vecchio edificio dell’INPS i cui lavori di demolizione
+                        sono iniziati nel settembre 2017...</p>
                     <div class="interaction">
                         <a href="article.php" class="button_article">Continua a leggere</a>
                     </div>
                 </div>
                 <div class="pic_card_container">
-                    <img class="pic_card" src="img-samples/photo-1579194044198-cdd94102ef46.jpg">
+                    <img class="pic_card" src="img-samples/gioia-22-1.jpg.jpg">
                 </div>
                 <div class="green_card"></div>
                 <div class="pink_card"></div>
             </div>
+
             <div class="container_article ca_2" data-aos="fade-up" data-aos-duration="600">
                 <div class="text_card">
                     <div class="tags">
-                        <p>Autore</p>
+                        <p>di <a href="about.php">Giuseppe Figliuolo</a></p>
 
                     </div>
-                    <h2>Titolo articolo di Spazi Sostenibili</h2>
-                    <p class="text_card_p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit modi expedita quod aut, nihil iste, hic quidem laborum nesciunt nemo nisi totam omnis, animi aspernatur dolorem ex. Aperiam, accusamus aliquid. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil adipisci quam fuga architecto soluta, velit quaerat libero nostrum hic vero perspiciatis labore doloribus aut doloremque quod, vitae dolore saepe aliquid.</p>
+                    <h2>Il nuovo
+                        Parco Monsanto:
+                        120 ettari di biodiversità</h2>
+                    <p class="text_card_p">Il sindaco di Milano Giuseppe Sala dichiara guerra allo smog. Con il blocco totale del traffico previsto per domenica 2 febbraio, ma anche con una crociata al riscaldamento che inquina e, in particolare, contro le caldaie a gasolio. E se per l’assessore lombardo all’Ambiente <span>Raffaele Cattaneo</span>, lo stop alle...</p>
                     <div class="interaction">
                         <a href="article.php" class="button_article">Continua a leggere</a>
                     </div>
                 </div>
                 <div class="pic_card_container">
-                    <img class="pic_card" src="img-samples/photo-1579126999978-eac334843123.jpg">
+                    <img class="pic_card" src="img/sample_greenpark.jpg">
                 </div>
                 <div class="green_card"></div>
                 <div class="pink_card"></div>
@@ -60,39 +66,34 @@
         <div class="gallery_posts" data-aos="fade-up" data-aos-duration="600">
             <a href="gallery.php" class="gallery_item gallery_item_1">
                 <div class="gallery_cover"></div>
-                <p class="figcaption">Nome Gallery 1</p>
-                <img src="img-samples/photo-1579126999978-eac334843123.jpg" alt="Gallery 1" class="gallery_img">
+                <p class="figcaption">RESIDENZE HADID</p>
+                <img src="img-samples/hadid-preview.jpg" alt="Gallery 1" class="gallery_img">
             </a>
 
             <a href="gallery.php" class="gallery_item gallery_item_2">
                 <div class="gallery_cover"></div>
-                <p class="figcaption">Nome Gallery 2</p>
-                <img src="img-samples/photo-1579160855213-a6e60979eecf.jpg" alt="Gallery 2" class="gallery_img">
+                <p class="figcaption">TORRE SOLARIA</p>
+                <img src="img-samples/solaria-preview.jpg" alt="Gallery 2" class="gallery_img">
             </a>
 
             <a href="gallery.php" class="gallery_item gallery_item_3">
                 <div class="gallery_cover"></div>
-                <p class="figcaption">Nome Gallery 3</p>
-                <img src="img-samples/photo-1579189880841-43ddba8d8efd.jpg" alt="Gallery 3" class="gallery_img">
+                <p class="figcaption">TORRE ISOZAKI</p>
+                <img src="img-samples/isozaki-preview.jpg" alt="Gallery 3" class="gallery_img">
             </a>
 
             <a href="gallery.php" class="gallery_item gallery_item_4">
                 <div class="gallery_cover"></div>
-                <p class="figcaption">Nome Gallery 4</p>
-                <img src="img-samples/photo-1579191748111-90d7f89fe214 (1).jpg" alt="Gallery 4" class="gallery_img">
+                <p class="figcaption">BOSCO VERTICALE</p>
+                <img src="img-samples/bv-preview.jpg" alt="Gallery 4" class="gallery_img">
             </a>
 
             <a href="gallery.php" class="gallery_item gallery_item_5">
                 <div class="gallery_cover"></div>
-                <p class="figcaption">Nome Gallery 5</p>
-                <img src="img-samples/photo-1579194044198-cdd94102ef46.jpg" alt="Gallery 5" class="gallery_img">
+                <p class="figcaption">TORRE UNICREDIT</p>
+                <img src="img-samples/unicredit-preview.jpg" alt="Gallery 5" class="gallery_img">
             </a>
 
-            <a href="gallery.php" class="gallery_item gallery_item_6">
-                <div class="gallery_cover"></div>
-                <p class="figcaption">Nome Gallery 6</p>
-                <img src="img-samples/photo-1579164618222-dfb28558a476.jpg" alt="Gallery 5" class="gallery_img">
-            </a>
             
         </div>
     </section>
@@ -101,17 +102,17 @@
             <div class="container_article ca_3" data-aos="fade-up" data-aos-duration="600">
                 <div class="text_card">
                     <div class="tags">
-                        <p>Autore</p>
+                        <p>di <a href="about.php">Giorgio Colombo</a></p>
 
                     </div>
-                    <h2>Titolo articolo di Spazi Sostenibili</h2>
-                    <p class="text_card_p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit modi expedita quod aut, nihil iste, hic quidem laborum nesciunt nemo nisi totam omnis, animi aspernatur dolorem ex. Aperiam, accusamus aliquid. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil adipisci quam fuga architecto soluta, velit quaerat libero nostrum hic vero perspiciatis labore doloribus aut doloremque quod, vitae dolore saepe aliquid.</p>
+                    <h2>Salone del Mobile, bellezza e sostenibilità</h2>
+                    <p class="text_card_p">Innovativo, sostenibile, virtuoso. Il Salone del Mobile Milano, alla prova Coronavirus, che l’Oms ha definito "emergenza globale", sarà ancora una volta, dal 21 al 26 aprile, in Fiera Milano a Rho, il palcoscenico per mostrare al mondo le novità, i progressi fatti da creativi, designer, brand e aziende del made in Italy...</p>
                     <div class="interaction">
                         <a href="article.php" class="button_article">Continua a leggere</a>
                     </div>
                 </div>
                 <div class="pic_card_container">
-                    <img class="pic_card" src="img-samples/photo-1579194044198-cdd94102ef46.jpg">
+                    <img class="pic_card" src="img-samples/violet-singapore.jpg">
                 </div>
                 <div class="green_card"></div>
                 <div class="pink_card"></div>
@@ -119,16 +120,16 @@
             <div class="container_article ca_4" data-aos="fade-up" data-aos-duration="600">
                 <div class="text_card">
                     <div class="tags">
-                        <p>Autore</p>
+                        <p>di <a href="about.php">Giuseppe Figliuolo</a></p>
                     </div>
-                    <h2>Titolo articolo di Spazi Sostenibili</h2>
-                    <p class="text_card_p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit modi expedita quod aut, nihil iste, hic quidem laborum nesciunt nemo nisi totam omnis, animi aspernatur dolorem ex. Aperiam, accusamus aliquid. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil adipisci quam fuga architecto soluta, velit quaerat libero nostrum hic vero perspiciatis labore doloribus aut doloremque quod, vitae dolore saepe aliquid.</p>
+                    <h2>A Milano è di scena la sostenibilità secondo Herno</h2>
+                    <p class="text_card_p">È un allestimento che mette in luce l’aspetto sostenibile, quello scelto dal Herno per l’outerwear del prossimo autunno-inverno. Nelle spazio milanese del brand, in via Solari, i capi della linea...</p>
                     <div class="interaction">
                         <a href="article.php" class="button_article">Continua a leggere</a>
                     </div>
                 </div>
                 <div class="pic_card_container">
-                    <img class="pic_card" src="img-samples/photo-1579126999978-eac334843123.jpg">
+                    <img class="pic_card" src="img-samples/amber-building.jpg">
                 </div>
                 <div class="green_card"></div>
                 <div class="pink_card"></div>
